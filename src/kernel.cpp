473b0d6158120d6b3bec572c2c1a2f6caa3522cb
Hser2bio <hser2bio@icloud.com>
2022-10-31 12:09:59 +0100

    ++

--- src/kernel.cpp
+++ src/kernel.cpp
@@ -1,7 +1,7 @@
 // Copyright (c) 2011-2013 The PPCoin developers
 // Copyright (c) 2013-2014 The NovaCoin Developers
 // Copyright (c) 2014-2018 The BlackCoin Developers
-// Copyright (c) 2015-2020 The PIVX developers
+// Copyright (c) 2015-2020 The CARI developers
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
@@ -17,8 +17,6 @@
 #include "validation.h"
 #include "zcarichain.h"
 #include "zcari/zpos.h"
-
-#include <boost/assign/list_of.hpp>
 
 /**
  * CStakeKernel Constructor
--- src/qt/cari/settings/settingsfaqwidget.cpp
+++ src/qt/cari/settings/settingsfaqwidget.cpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2019 The PIVX developers
+// Copyright (c) 2019 The CARI developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
@@ -64,6 +64,91 @@ SettingsFaqWidget::SettingsFaqWidget(CARIGUI *parent) :
             }, "btn-faq-options");
 
     ui->labelContent_Support->setOpenExternalLinks(true);
+
+    // Set FAQ content strings
+    QString introContent = formatFAQContent(
+        formatFAQParagraph(
+            tr("CARI is a form of digital online money using blockchain technology "
+               "that can be easily transferred globally, instantly, and with near "
+               "zero fees. CARI incorporates market leading security & "
+               "privacy and is also the first PoS (Proof of Stake) Cryptocurrency "
+               "to implement Sapling(SHIELD), a zk-SNARKs based privacy protocol.")) +
+        formatFAQParagraph(
+            tr("CARI utilizes a Proof of Stake (PoS) consensus system algorithm, "
+               "allowing all owners of CARI to participate in earning block rewards "
+               "while securing the network with full node wallets, as well as to "
+               "run Masternodes to create and vote on proposals.")));
+    ui->labelContent_Intro->setText(introContent);
+
+    QString unspendableCARIContent = formatFAQContent(
+        formatFAQParagraph(
+            tr("Newly received CARI requires 6 confirmations on the network "
+               "to become eligible for spending which can take ~6 minutes.")) +
+        formatFAQParagraph(
+            tr("Your CARI wallet also needs to be completely synchronized "
+               "to see and spend balances on the network.")));
+    ui->labelContent_UnspendableCARI->setText(unspendableCARIContent);
+
+    QString stakeContent = formatFAQContent(
+        formatFAQOrderedList(
+            formatFAQListItem(tr("Make sure your wallet is completely synchronized and you are using the latest release.")) +
+            formatFAQListItem(tr("You must have a balance of CARI with a minimum of 600 confirmations.")) +
+            formatFAQListItem(tr("Your wallet must stay online and be unlocked for staking purposes.")) +
+            formatFAQListItem(tr("Once all those steps are followed staking should be enabled."))) +
+        formatFAQParagraph(
+            tr("You can see the status of staking in the wallet by mousing over the "
+               "package icon in the row on the top left of the wallet interface. The "
+               "package will be lit up and will state \"Staking Enabled\" to indicate "
+               "it is staking. Using the command line interface (%1); the command %2 "
+               "will confirm that staking is active.")
+                .arg("cari-core", "<span style=\"font-style:italic\">getstakingstatus</span>")));
+    ui->labelContent_Stake->setText(stakeContent);
+
+    QString supportContent = formatFAQContent(
+        formatFAQParagraph(
+            tr("We have support channels in most of our official chat groups, for example %1")
+                .arg("<a style='color: #b088ff' href='https://discord.CARI.org'>" + tr("#support in our Discord") + "</a>.")));
+    ui->labelContent_Support->setText(supportContent);
+
+    QString masternodeContent = formatFAQContent(
+        formatFAQParagraph(
+            tr("A masternode is a computer running a full node CARI core wallet with a "
+               "requirement of 10,000 CARI secured collateral to provide extra services "
+               "to the network and in return, receive a portion of the block reward "
+               "regularly. These services include:") +
+            formatFAQUnorderedList(
+                formatFAQListItem(tr("A decentralized governance (Proposal Voting)")) +
+                formatFAQListItem(tr("A decentralized budgeting system (Treasury)")) +
+                formatFAQListItem(tr("Validation of transactions within each block")) +
+                formatFAQListItem(tr("Act as an additional full node in the network")))) +
+        formatFAQParagraph(
+            tr("For providing such services, masternodes are also paid a certain portion "
+               "of reward for each block. This can serve as a passive income to the "
+               "masternode owners minus their running cost.")) +
+        formatFAQParagraph(
+            tr("Masternode Perks:") +
+            formatFAQUnorderedList(
+                formatFAQListItem(tr("Participate in CARI Governance")) +
+                formatFAQListItem(tr("Earn Masternode Rewards")) +
+                formatFAQListItem(tr("Commodity option for future sale")) +
+                formatFAQListItem(tr("Help secure the CARI network")))) +
+        formatFAQParagraph(
+            tr("Requirements:") +
+            formatFAQUnorderedList(
+                formatFAQListItem(tr("10,000 CARI per single Masternode instance")) +
+                formatFAQListItem(tr("Must be stored in a core wallet")) +
+                formatFAQListItem(tr("Need dedicated IP address")) +
+                formatFAQListItem(tr("Masternode wallet to remain online")))));
+    ui->labelContent_Masternode->setText(masternodeContent);
+
+    QString mNControllerContent = formatFAQContent(
+        formatFAQParagraph(
+            tr("A Masternode Controller wallet is where the 10,000 CARI collateral "
+               "can reside during a Controller-Remote masternode setup. It is a wallet "
+               "that can activate the remote masternode wallet(s) and allows you to keep "
+               "your collateral coins offline while the remote masternode remains online.")));
+    ui->labelContent_MNController->setText(mNControllerContent);
+
 
     // Exit button
     setCssProperty(ui->pushButtonExit, "btn-faq-exit");
